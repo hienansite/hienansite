cd9faa44fc42c53a2d8fe13a6777705c9e1841b7
AnNH19 <AnNH19@users.noreply.github.com>
2024-01-15 20:00:39 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
+import { TodDoList } from './features/todo/index';
+import './App.css';
+// APP: <Counter /> 
+
+function App() {
+  return <>
+    <TodDoList /> 
+  </>;
+}
+
+export default App;
